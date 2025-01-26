921f237138bc7ba089ee97c839ceae5de0a01b4c
ZETA7 <zeta0707@gmail.com>
2025-01-26 09:12:54 +0900

    Add code for checking Rebearm home position
     Need to fix torque off when starts

--- hello-world.py
+++ hello-world.py
@@ -1,22 +1,85 @@
-from math import sin, cos
-from pylx16a.lx16a import *
+from lx16a import *
 import time
+import myconfig
 
 LX16A.initialize("/dev/ttyUSB0", 0.1)
+
+#assume -120~120 => 0~240
+def a2c(angle):
+    return angle + 120.0
 
 try:
     servo1 = LX16A(1)
     servo2 = LX16A(2)
+    servo3 = LX16A(3)
+    servo4 = LX16A(4)
+    servo5 = LX16A(5)
+    servo6 = LX16A(6)
     servo1.set_angle_limits(0, 240)
     servo2.set_angle_limits(0, 240)
+    servo3.set_angle_limits(0, 240)
+    servo4.set_angle_limits(0, 240)
+    servo5.set_angle_limits(0, 240)
+    servo6.set_angle_limits(0, 240)
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
     quit()
 
-t = 0
+#check home operation
 while True:
-    servo1.move(sin(t) * 60 + 60)
-    servo2.move(cos(t) * 60 + 60)
+    print("Start Home")
+    val = a2c(myconfig.GRIPPER_OPEN)
+    servo6.move(val, time=500)
+    time.sleep(1)
+    angle = servo6.get_commanded_angle() 
+    print('Gr:',val, angle)
 
-    time.sleep(0.05)
-    t += 0.1
+    val = a2c(myconfig.MOTOR4_HOME + 15.0)
+    servo4.move(val, time=500)
+    time.sleep(1)
+    angle = servo4.get_commanded_angle() 
+    print('M4:',val, angle)
+
+    val = a2c(myconfig.MOTOR3_HOME - 10.0)
+    servo3.move(val, time=500)
+    time.sleep(1)
+    angle = servo3.get_commanded_angle() 
+    print('M3:',val, angle)
+
+    val = a2c(myconfig.MOTOR2_HOME + 10.0)
+    servo2.move(val, time=500)
+    time.sleep(1)
+    angle = servo2.get_commanded_angle() 
+    print('M2:',val, angle)
+
+    val = a2c(myconfig.MOTOR4_HOME )
+    servo4.move(val, time=500)
+    time.sleep(1)
+    angle = servo4.get_commanded_angle() 
+    print('M4:',val, angle)
+
+    val = a2c(myconfig.MOTOR5_HOME )
+    servo5.move(val, time=500)
+    time.sleep(1)
+    angle = servo5.get_commanded_angle() 
+    print('M5:',val, angle)
+
+    val = a2c(myconfig.MOTOR3_HOME)
+    servo3.move(val, time=500)
+    time.sleep(1)
+    angle = servo3.get_commanded_angle() 
+    print('M3:',val, angle)
+
+    val = a2c(myconfig.MOTOR2_HOME)
+    servo2.move(val, time=500)
+    time.sleep(1)
+    angle = servo2.get_commanded_angle() 
+    print('M2:',val, angle)
+
+    val = a2c(myconfig.MOTOR1_HOME)
+    servo1.move(val, time=500)
+    time.sleep(1)
+    angle = servo1.get_commanded_angle() 
+    print('M1:',val, angle)
+
+    print("Done Home")
---
+++ myconfig.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Author: ChangWhan Lee
+MOTOR_NOMOVE = 360.0
+MOTOR_RIGHT = -90.0
+
+#turn left or right of base
+MOTOR1_ZERO = 0.0
+MOTOR1_HOME = 0.0
+MOTOR1_OFF = MOTOR1_HOME
+MOTOR1_MAX = 90.0
+MOTOR1_MIN = -90.0
+MOTOR1_PLACE1 = MOTOR1_MAX
+MOTOR1_PLACE2 = MOTOR1_MIN
+MOTOR1_PLACE3 = 45.0
+MOTOR1_PLACE4 = -45.0
+
+#forward or backward
+#roll, 3DoF
+MOTOR2_ZERO = 0.0
+MOTOR2_HOME = 10.0
+MOTOR2_PICKUP = -10.0
+MOTOR2_OFF = 90.0
+MOTOR2_MAX = 30.0
+MOTOR2_MIN = (MOTOR2_HOME - 65.0)
+
+MOTOR3_ZERO = 0.0
+MOTOR3_HOME = -75.0
+MOTOR3_PICKUP = -86.0
+MOTOR3_OFF = -110.0
+MOTOR3_MAX  = (MOTOR3_HOME + 40.0)
+MOTOR3_MIN  =  -120.0
+
+MOTOR4_ZERO = 0.0
+MOTOR4_HOME = -104.0
+MOTOR4_PICKUP = -78.0
+MOTOR4_OFF = -100.0
+MOTOR4_MAX  = (MOTOR4_HOME + 40.0)
+MOTOR4_MIN  = -120.0
+
+MOTOR5_ZERO = 0.0
+MOTOR5_HOME = -90.0
+MOTOR5_PICKUP = (MOTOR5_HOME + 0.0)
+MOTOR5_OFF = 0.0
+MOTOR5_MAX  = 90.0
+MOTOR5_MIN  = -90.0
+
+GRIPPER_CLOSE = -80.0
+GRIPPER_OPEN = -66.0
+GRIPPER_MIN = GRIPPER_CLOSE
+GRIPPER_MAX = GRIPPER_OPEN
+
+#yolo_chase or blob_chase
+PICTURE_SIZE_X = 640.0
+PICTURE_SIZE_Y = 480.0
+
+CB_FREQ = 0.1
+
+#teleop_joy parameter
+MAX_CHAT = 5           #for protecting jostick's button chattering
+TIMER_JOY = 0.1         #for protecting jostick's button chattering
+CONT_JOY = 3            #don't move untill CONT_JOY
+
+#teleop_keyboard parameter
+CONT_KEY = 3            #don't move untill CONT_KEY*100ms
+
+#human_guide parameter
+TIMER_HGUIDE = 0.2      #how frequent angle capture
